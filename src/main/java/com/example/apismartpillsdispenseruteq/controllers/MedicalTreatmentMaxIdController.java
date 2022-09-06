61556f10125e03c57a2b15367c15f181767d8666
Jonathan cevallos Mayorga <45270300+JonathanCevallos@users.noreply.github.com>
2022-09-05 19:33:56 -0500

    SE AGREGÓ EL MÉTODO QUE DEVUELVE EL ID MAXIMO DE MEDICAL-TREATMENT

--- src/main/java/com/example/apismartpillsdispenseruteq/controllers/MedicalTreatmentMaxIdController.java
+++ src/main/java/com/example/apismartpillsdispenseruteq/controllers/MedicalTreatmentMaxIdController.java
@@ -1,13 +1,8 @@
 package com.example.apismartpillsdispenseruteq.controllers;
 
-import com.example.apismartpillsdispenseruteq.entity.MedicalTreatment;
 import com.example.apismartpillsdispenseruteq.services.MedicalTreatmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/medical-treatment/max-id")
