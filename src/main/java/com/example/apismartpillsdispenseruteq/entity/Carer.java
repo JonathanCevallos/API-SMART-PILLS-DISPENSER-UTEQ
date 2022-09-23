d23120fa41fd2e6f5f02b60aaca587cbedf3b754
Jonathan cevallos Mayorga <45270300+JonathanCevallos@users.noreply.github.com>
2022-09-23 08:33:06 -0500

    Se actualizo la entidad Carer.java

--- src/main/java/com/example/apismartpillsdispenseruteq/entity/Carer.java
+++ src/main/java/com/example/apismartpillsdispenseruteq/entity/Carer.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
